80340b82f3933876fe8a69c866b2175e4158e24a
Matthews offen <matthewsoffen2@gmail.com>
2024-12-28 11:38:59 +0200

    start of modular calls
    typically, test the design pattern object using a specified method call.

--- src/main/java/org/craft/designpatterns/structural/Main.java
+++ src/main/java/org/craft/designpatterns/structural/Main.java
@@ -4,12 +4,7 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        Xpay client = new XpayImpl();
-        client.setCustomerName("Matthews");
-        client.setCardCVVNo((short) 29);
-        client.setAmount(2_000.0);
-
-        transact(new Adaptor(client));
+        testFacade();
     }
 
     //initially this used xpay, but now it has been changed to use payd,
@@ -19,4 +14,21 @@ public class Main {
                 " has paid " + details.getTotalAmount() +
                 " on " + new Date());
 ;    }
+
+    static void testAdaptor(){
+        Xpay client = new XpayImpl();
+        client.setCustomerName("Matthews");
+        client.setCardCVVNo((short) 29);
+        client.setAmount(2_000.0);
+
+        transact(new Adaptor(client));
+    }
+
+    static  void testFacade(){
+        //instead of creating an object and independently calling the start and
+        //stop methods of the object in the predefined sequense, you can just use the facade.
+        Facade scheduleServerFacade = new Facade(new ScheduleServer());
+        scheduleServerFacade.startServer();
+        scheduleServerFacade.stopServer();
+    }
 }
